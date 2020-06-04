1dbdbc816e121b064d6baec72b30a5d2644f8eb2
Vadim Kandrushin <vkandrushin@gmail.com>
2020-06-04 17:36:25 +0700

    2.1.4

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 
 [assembly: AssemblyCompanyAttribute("Helios Framework")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Aaron Stannard 2013-2015")]
-[assembly: AssemblyVersionAttribute("2.1.2")]
-[assembly: AssemblyFileVersionAttribute("2.1.2")]
+[assembly: AssemblyVersionAttribute("2.1.4")]
+[assembly: AssemblyFileVersionAttribute("2.1.4")]
